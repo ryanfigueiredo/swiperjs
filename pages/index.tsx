b3ddbd95e9cb1a0031378a53163c59085e1adcb5
ryanfigueiredo <ryanfigueiredo@hotmail.com>
2022-03-13 15:11:33 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+const Home: NextPage = () => {
+  return (
+    <Swiper
+    spaceBetween={10}
+    initialSlide={0}
+    loop={true}
+    slidesPerView={1}
+    centeredSlides={true}
+    grabCursor={true}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  )
+}
+
+export default Home
